27785cace39e8f7d0fdb9ed515845545515f2bec
Bradw <83971386+BJWRD@users.noreply.github.com>
2023-01-16 09:19:53 +0000

    Description Updates

--- variables.tf
+++ variables.tf
@@ -3,20 +3,20 @@
 ################################################################################
 
 variable "region" {
-  type        = string
   description = "AWS Region"
+  type        = string
   default     = "eu-west-2"
 }
 
 variable "vpc_id" {
-  type        = string
   description = "The VPC to deploy into"
+  type        = string
   default     = "vpc-025a976bcbce78ab9"
 }
 
 variable "private_subnet" {
+  description = "A list of private subnets inside the VPC"
   type        = map(string)
-  description = "Private Subnets"
   default = {
     "eu-west-2a" = "10.0.1.0/24",
     "eu-west-2b" = "10.0.2.0/24",
@@ -25,8 +25,8 @@ variable "private_subnet" {
 }
 
 variable "public_subnet" {
+  description = "A list of public subnets inside the VPC"
   type        = map(string)
-  description = "Public Subnets"
   default = {
     "eu-west-2a" = "10.0.4.0/24",
     "eu-west-2b" = "10.0.5.0/24",
@@ -35,35 +35,32 @@ variable "public_subnet" {
 }
 
 variable "availability_zones" {
+  description = "A list of Availability Zones used"
   type        = list(string)
-  description = "List of Availability Zones used"
   default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 }
 
-variable "subnet_id" {
-  type    = string
-  default = "aws_subnet.private.id"
-}
-
 variable "aws_internet_gateway" {
+  description = "The AWS Internet Gateway resource"
   type    = string
   default = "aws_internet_gateway.main"
 }
 
 variable "aws_eip" {
+  description = "The EIP ID"
   type = string
   default = "aws_eip.main.id"
 }
 
 variable "cidr_block" {
-  type        = string
   description = "CIDR Block to allow traffic via"
+  type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "security_group" {
-  type        = string
   description = "Name of the EC2 Security Group"
+  type        = string
   default     = "SG-App"
 }
 
@@ -71,4 +68,10 @@ variable "vpc_security_group_ids" {
   description = "A list of security group IDs to associate with"
   type        = list(string)
   default     = null
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type    = string
+  default = "aws_subnet.private.id"
 }
