1f6ec43dc76f69d7efee72f5d69542f171260b13
BJWRD <brad.whitcomb@sky.com>
2023-01-17 15:09:41 +0000

    Variable Updates

--- ec2.tf
+++ ec2.tf
@@ -6,15 +6,12 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "EC2"
-
-  ami                    = "ami-084e8c05825742534"
-  instance_type          = "t2.nano"
-  monitoring             = true
+  name                   = var.ec2_name
+  ami                    = var.ami_type
+  instance_type          = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id              = var.subnet_id
-  #key_name - allows private key (.pem file) to be used with ec2 instance
-  key_name = "terraform"
-
+  for_each                = var.private_subnet
+  subnet_id              = aws_subnet.private[each.key].id
+  
   tags = merge(local.required_tags, { Name = "EC2-Instance" })
 }
