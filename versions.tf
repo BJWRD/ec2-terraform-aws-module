74c2c588b7ad782422e1e888c9ced394655a257f
BJWRD <brad.whitcomb@sky.com>
2023-01-17 15:10:46 +0000

    S3 Bucket Inclusion

--- versions.tf
+++ versions.tf
@@ -13,8 +13,8 @@ terraform {
   }
 
   backend "s3" {
-      bucket = "tfstate-dev-bucket"
-      key    = "terraform.tfstate"
-      region = "eu-west-2"
+    bucket = "tfstate-dev-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
   }
 }
